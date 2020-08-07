2087cc39e057d78b1094fabc705c49794ef19521
annia valdes <millerl.mb79@gmail.com>
2020-08-07 07:53:15 -0500

    create a file for work all about department.create function sql  showDepartments and  addDepartment

---
+++ department.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const db = require('./db/database');
+
+
+
+
+class Department {
+    constructor() {
+
+    }
+
+    showDepartments() {
+        db.query(`SELECT * FROM department`, (err, res) => {
+            if (err) throw err;
+    
+            if (res.length > 0) {
+               
+                console.table(res);
+            }
+    
+        });
+    
+    }
+
+    addDepartment() {
+        inquirer.prompt([
+            //manager name
+            {
+                type: 'input',
+                name: 'nameDpto',
+                message: 'What is your deparment name?',
+                validate: name => {           //validation the entry
+                    if (name) {
+                        return true;
+                    } else {
+                        console.log('\n Please enter team manager’s name!');
+                        return false;
+                    }
+                }
+            }
+        ])
+            .then(anserw => {
+                let nameDepartment = anserw.nameDpto;
+                db.query('INSERT INTO department SET name=? ', [nameDepartment], (err, res) => {
+                    if (err) throw err;
+                    if (res.afectedRows > 0) {
+                        console.log("succesfully insert")
+                    }
+                })
+            })
+    }
+
+}
+
+module.exports = new Department();
