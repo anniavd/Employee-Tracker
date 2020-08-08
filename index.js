a1bd0bb391cfb2f62e26a98f20f65f3e52e1de62
annia valdes <millerl.mb79@gmail.com>
2020-08-08 15:19:03 -0500

    deleted a file database and employee

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const dept = require('./department');
-const role=require('./role');
-const employee=require('./employee')
-const db = require('./db/database');
+const cTable = require('console.table');
+let deptoChoice = [];
+let roleChoice = [];
+let employeName=[];
 
-function showOptions() {
+
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  // Your MySQL username
+  user: 'root',
+  // Your MySQL password
+  password: 'Basedatos20*',
+  database: 'department_db'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId + '\n');
+  menu();
+});
+
+
+
+
+function menu() {
   console.log("\n** Select options **\n");
 
   inquirer.prompt([
@@ -19,39 +42,39 @@ function showOptions() {
         'View all the Role',
         'Add a Role',
         'Delete a Role',
-        'View all the Employee',   
+        'View all the Employee',
         'Add a Employee',
         'Delete a Employee',
         'Update a employee role',
-        'Quit'           
+        'Quit'
       ]
 
     }
   ]).then(options => {
     switch (options.selectOptions) {
       case 'View all the Departments':
-        dept.showDepartments();
+        showDepartments();
         break;
       case 'Add a Department':
-        dept.addDepartment();
+        addDepartment();
         break;
       case 'Delete a Department':
-        dept.deleteApartment();
+        deleteApartment();
         break;
-      case 'View all the Role':  
-      role.showRoles();     
+      case 'View all the Role':
+        showRoles();
         break;
-      case 'Add a Role': 
-       role.addRole();      
+      case 'Add a Role':
+        addRole();
         break;
-      case'Delete a Role':
-      role.deleteRole(); 
+      case 'Delete a Role':
+        role.deleteRole();
         break;
       case 'View all the Employee':
-        employee.showEmployees();
+        showEmployees();
         break;
       case 'Add a Employee':
-        employee.addEmployee();
+        addEmployee();
         break;
       case 'Delete a Employee':
         employee.deleteEmployee();
@@ -59,10 +82,266 @@ function showOptions() {
       case 'Update a employee role':
         break;
       default:
-      db.end();
+        connection.end();
     }
   });
-  
+
 }
 
-module.exports=showOptions();
+
+// show all department
+function showDepartments() {
+  //sql consult select
+  connection.query(`SELECT * FROM department`, (err, res) => {
+    if (err) throw err;
+
+    if (res.length > 0) {
+      console.log('\n')
+      console.log('Departments')
+      console.log('\n')
+      console.table(res);
+    }
+    menu();
+  });
+
+}
+// show all the role info 
+function showRoles() {
+  //sql consult select
+  connection.query(`SELECT role.title AS job_title,role.id,department.name AS department_name,role.salary  FROM  role LEFT JOIN department ON role.department_id=department.id`,
+    (err, res) => {
+
+      if (err) throw err;
+
+      if (res.length > 0) {
+        console.log('\n')
+        console.log('Roles')
+        console.log('\n')
+        console.table(res);
+      }
+      menu();
+    });
+}
+// show all the employee info
+function showEmployees() {
+  //sql consult select
+  connection.query(`SELECT employee.id, employee.first_name,employee.last_name,role.title AS job_title,role.salary,
+       CONCAT(manager.first_name ," ", manager.last_name) AS Manager FROM  employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN employee  manager ON manager.id = employee.manager_id`, (err, res) => {
+   // console.log(res);
+
+    if (err) throw err;
+
+    if (res.length > 0) {
+      console.log('\n')
+      console.log('Employees')
+      console.log('\n')
+      console.table(res);
+    }
+    menu();
+  });
+
+}
+
+
+
+//add a department to the datebase
+function addDepartment() {
+
+  inquirer.prompt([
+
+    {
+      type: 'input',
+      name: 'nameDpto',
+      message: 'What is your department name?',
+      validate: name => {           //validation the entry
+        if (name) {
+          return true;
+        } else {
+          console.log('\n Please enter a department name!');
+          return false;
+        }
+      }
+    }
+  ])
+    .then(anserw => {
+      let nameDepartment = anserw.nameDpto;
+      //sql consult insert
+      connection.query('INSERT INTO department SET name=? ', [nameDepartment], (err, res) => {
+        if (err) throw err;
+
+        console.log(res.affectedRows + ' department inserted!\n');
+        menu();
+      })
+    })
+
+}
+function deleteApartment() {
+
+  inquirer.prompt([
+
+    {
+      type: 'input',
+      name: 'deptoId',
+      message: 'What is the department id ?',
+      validate: idInput => {           //validation the entry
+        if (idInput) {
+          return true;
+        } else {
+          console.log('\n Please enter the department id!');
+          return false;
+        }
+      }
+    }
+  ])
+    .then(anserw => {
+      let deleteId = anserw.deptoId;
+      //sql consult delete
+      connection.query('DELETE FROM department WHERE id=? ', [deleteId], (err, res) => {
+        if (err) throw err;
+
+        console.log(res.affectedRows + ' department delete!\n');
+        menu();
+      })
+    })
+}
+
+function helperArray() {
+  connection.query(`SELECT * FROM department `, (err, res) => {
+
+    if (err) throw err;   
+    res.forEach(dpto => {
+      deptoChoice.push({ name:dpto.name, value: dpto.id })
+    })
+    console.log('metodo', deptoChoice)
+    return deptoChoice;
+  });
+}
+// add a role info to the date base
+function addRole() {
+  //the function back a array with all the departments name
+  helperArray();
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your rol name?',
+      validate: name => {           //validation the entry
+        if (name) {
+          return true;
+        } else {
+          console.log('\n Please enter a title rol!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: 'What is the salary rol?',
+      validate: salaryInput => {           //validation the entry
+        if (salaryInput) {
+          return true;
+        } else {
+          console.log('\n Please enter a salary rol!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'list',
+      name: 'dptoId',
+      message: 'Select the department for that rol?',
+      choices: deptoChoice
+
+    }
+  ])
+    .then(anserw => {
+      let title = anserw.title;
+      let salary = anserw.salary;
+      let id = anserw.dptoId;
+      console.log('ID', id)
+
+
+      //query insert a role
+      connection.query('INSERT INTO role SET title=?,salary=?,department_id=? ', [title, salary, id], (err, res) => {
+        if (err) throw err;
+        console.log(res.affectedRows + ' role inserted!\n');
+        menu();
+      })
+    })
+}
+
+
+
+
+function helperEmployee() {
+ connection.query(`SELECT role.title,role.id FROM role `, (err, res) => {
+
+      if (err) throw err;     
+      res.forEach(roles => {
+        roleChoice.push({name:roles.title,value:roles.id})
+      })
+      console.log('titulo y id',roleChoice)
+      return roleChoice;
+  });
+}
+
+//add a employe to the date base
+function addEmployee() {
+  helperEmployee();
+  inquirer.prompt([
+
+      {
+          type: 'input',
+          name: 'name',
+          message: 'What is the employee\manager name?',
+          validate: name => {           //validation the entry
+              if (name) {
+                  return true;
+              } else {
+                  console.log('\n Please enter the name!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'lastName',
+          message: 'What is the employee\ manager last name?',
+          validate: lastnameInput => {           //validation the entry
+              if (lastnameInput) {
+                  return true;
+              } else {
+                  console.log('\n Please enter the last name!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'list',
+          name: 'selectRole',
+          message: 'Select a role for a employee',
+          choices: roleChoice
+      },
+      {
+          type: 'input',
+          name: 'magId',
+          message: 'What is the manager id?'
+
+      }
+  ])
+      .then(anserw => {
+          //taking the value entry for the prompt
+          let name = anserw.name;
+          let last = anserw.lastName;
+          let roleIdEmp = anserw.selectRole;
+          //manager id can be null to
+          let managerId = anserw.magId || null;
+
+          //sql consult insert  a employee
+          connection.query('INSERT INTO employee SET first_name=?,last_name=?,role_id=?,manager_id=? ', [name, last, roleIdEmp, managerId], (err, res) => {
+              if (err) throw err;
+              console.log(res.affectedRows + ' employee inserted!\n');
+          })
+      })
+}
