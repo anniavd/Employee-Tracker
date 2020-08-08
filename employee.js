6c2b17cacd547af438fe8bd3dcd2b3e2b09654fc
annia valdes <millerl.mb79@gmail.com>
2020-08-07 20:05:44 -0500

    update showEmployee,now show the manager name for employee

--- employee.js
+++ employee.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const db = require('./db/database');
+const { query } = require('./db/database');
+const role = require('./role');
 
 
 
@@ -11,18 +13,25 @@ class Employee {
     //show employee info in a table
     showEmployees() {
         //sql consult select
-        db.query(`SELECT employee.id, employee.first_name,employee.last_name,
-        role.title AS job_title,role.salary FROM  employee LEFT JOIN role ON employee.role_id=role.id`, (err, res) => {
+        db.query(`SELECT employee.id, employee.first_name,employee.last_name,role.title AS job_title,role.salary,
+         CONCAT(manager.first_name ," ", manager.last_name) AS Manager FROM  employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN employee  manager ON manager.id = employee.manager_id`, (err, res) => {
+            console.log(res); 
+           
             if (err) throw err;
     
             if (res.length > 0) {
                
                 console.table(res);
-            }    
+            }   
+            
         });    
+        
     }
   //add a employe to the date base
    addEmployee () {
+    //  db.query(select role.title from role,(req,res))
+    //  let array=[];//
+//array=res
         inquirer.prompt([
             
             {
@@ -55,6 +64,7 @@ class Employee {
             type: 'input',
             name: 'roleId',
             message: 'What is the role id?',
+           // choices:array
             validate: roleIdinput => {           //validation the entry
                 if (roleIdinput ) {
                     return true;
--- index.js
+++ index.js
@@ -65,5 +65,4 @@ function showOptions() {
   
 }
 
-
-showOptions();
+module.exports=showOptions();
